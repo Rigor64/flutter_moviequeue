51f25a97d265c3d4a585b1cc8184e9219e9f04ce
Rigor64 <matteoleopizzi99@gmail.com>
2023-12-14 16:45:27 +0100

    changing files

--- Esercitazione2/android/app/src/main/kotlin/com/example/helloworld1/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.helloworld1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- liste/android/app/src/main/kotlin/com/example/liste/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.liste
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
